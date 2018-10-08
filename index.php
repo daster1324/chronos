b582636c4c6c82d5cad017017604e560403564a2
Javier García <jayru1234@gmail.com>
2018-10-08 19:20:14 +0200

    Reordenacion
    Estructura de carpetas cambiada

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Test</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/resources/css/bootstrap.min.css">
+
+    <style>
+        html,body {
+            height: 100%;
+            background-color: #fabada;
+        }
+    </style>
+  </head>
+  <body>    
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-5">Mostrar Opciones</div>
+            <div class="col-5"></div>
+        </div>
+    </div>
+    <div class="container-fluid h-100">
+        <div class="row justify-content-center h-100 bg-primary">
+            <div class="col-md-3 col-lg-2 hidden-md-down bg-danger" id="yellow">
+                XXXX
+            </div>
+            <div class="col-md-9 col-lg-10 bg-secondary">
+                Form Goes Here
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="/resources/js/jquery.min.js"></script>
+    <script src="/resources/js/popper.min.js"></script>
+    <script src="/resources/js/bootstrap.min.js"></script>
+  </body>
+</html>
