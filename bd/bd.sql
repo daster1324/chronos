17c393626432d7ab9680cd24cdab2146386e29ba
Javier <javieg02@ucm.es>
2019-03-25 06:37:28 +0100

    Nueva BBDD
    Añadida la tabla Itinerarios y adaptadas las tablas relacionadas.
    Eliminación de test

--- bd/bd.sql
+++ bd/bd.sql
@@ -1,12 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-12-2018 a las 23:16:09
--- Versión del servidor: 10.1.32-MariaDB
--- Versión de PHP: 7.0.30
+-- Tiempo de generación: 25-03-2019 a las 06:34:30
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.0.33
 
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -21,7 +22,7 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `chronos`
 --
-CREATE DATABASE IF NOT EXISTS `chronos` DEFAULT CHARACTER SET latin1 COLLATE latin1_spanish_ci;
+CREATE DATABASE IF NOT EXISTS `chronos` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish2_ci;
 USE `chronos`;
 
 -- --------------------------------------------------------
@@ -33,28 +34,22 @@ USE `chronos`;
 CREATE TABLE `asignaturas` (
   `id` int(10) NOT NULL,
   `id_carrera` int(2) NOT NULL,
-  `itinerario` varchar(5) COLLATE latin1_spanish_ci DEFAULT NULL,
-  `nombre` varchar(150) COLLATE latin1_spanish_ci NOT NULL,
-  `abreviatura` varchar(10) COLLATE latin1_spanish_ci DEFAULT NULL,
-  `curso` varchar(1) COLLATE latin1_spanish_ci NOT NULL,
+  `itinerario` int(2) DEFAULT NULL,
+  `nombre` varchar(150) COLLATE utf8_spanish2_ci NOT NULL,
+  `abreviatura` varchar(10) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `curso` varchar(1) COLLATE utf8_spanish2_ci NOT NULL,
   `id_departamento` int(2) NOT NULL,
   `id_departamento_dos` int(2) DEFAULT NULL,
   `creditos` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
---
--- Truncar tablas antes de insertar `asignaturas`
---
-
-TRUNCATE TABLE `asignaturas`;
 --
 -- Volcado de datos para la tabla `asignaturas`
 --
 
 INSERT INTO `asignaturas` (`id`, `id_carrera`, `itinerario`, `nombre`, `abreviatura`, `curso`, `id_departamento`, `id_departamento_dos`, `creditos`) VALUES
-(1, 1, 'ii', 'TEST', 'TST', '1', 1, NULL, 1),
-(2, 2, 'ii', 'test', 'tst', '1', 1, NULL, 12),
-(3, 2, 'ii', 'test', 'tst', '1', 1, NULL, 12);
+(1, 1, NULL, 'Cálculo', NULL, '1', 1, NULL, 12),
+(2, 1, NULL, 'Álgebra', NULL, '2', 3, 1, 6);
 
 -- --------------------------------------------------------
 
@@ -64,16 +59,20 @@ INSERT INTO `asignaturas` (`id`, `id_carrera`, `itinerario`, `nombre`, `abreviat
 
 CREATE TABLE `carreras` (
   `id` int(2) NOT NULL,
-  `nombre` varchar(150) COLLATE latin1_spanish_ci NOT NULL,
+  `nombre` varchar(150) COLLATE utf8_spanish2_ci NOT NULL,
   `id_facultad` int(2) NOT NULL,
   `id_facultad_dg` int(2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
--- Truncar tablas antes de insertar `carreras`
+-- Volcado de datos para la tabla `carreras`
 --
 
-TRUNCATE TABLE `carreras`;
+INSERT INTO `carreras` (`id`, `nombre`, `id_facultad`, `id_facultad_dg`) VALUES
+(1, 'Ingeniería Informática', 1, NULL),
+(2, 'Ingeniería del Software', 1, NULL),
+(3, 'Ingeniería de Computadores', 1, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -84,30 +83,19 @@ CREATE TABLE `clases` (
   `id` bigint(15) NOT NULL,
   `id_asignatura` int(10) NOT NULL,
   `cuatrimestre` int(1) NOT NULL,
-  `dia` varchar(1) COLLATE latin1_spanish_ci NOT NULL,
+  `dia` varchar(1) COLLATE utf8_spanish2_ci NOT NULL,
   `hora` int(2) NOT NULL,
-  `grupo` varchar(10) COLLATE latin1_spanish_ci NOT NULL,
+  `grupo` varchar(10) COLLATE utf8_spanish2_ci NOT NULL,
   `edificio` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
---
--- Truncar tablas antes de insertar `clases`
---
-
-TRUNCATE TABLE `clases`;
 --
 -- Volcado de datos para la tabla `clases`
 --
 
 INSERT INTO `clases` (`id`, `id_asignatura`, `cuatrimestre`, `dia`, `hora`, `grupo`, `edificio`) VALUES
-(1, 1, 1, 'l', 1, 'a', 1),
-(2, 2, 1, 'm', 2, 'a', 1),
-(3, 2, 2, 'm', 2, 'a', 1),
-(4, 2, 2, 'm', 2, 'a', 1),
-(5, 2, 2, 'm', 2, 'a', 1),
-(6, 2, 2, 'm', 2, 'a', 1),
-(7, 2, 2, 'm', 2, 'a', 1),
-(8, 2, 2, 'm', 2, 'a', 1);
+(1, 3, 2, 'j', 1, '2', 2),
+(2, 2, 1, 'm', 5, '1', 1);
 
 -- --------------------------------------------------------
 
@@ -117,15 +105,18 @@ INSERT INTO `clases` (`id`, `id_asignatura`, `cuatrimestre`, `dia`, `hora`, `gru
 
 CREATE TABLE `departamentos` (
   `id` int(2) NOT NULL,
-  `nombre` varchar(100) COLLATE latin1_spanish_ci NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
   `id_facultad` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
--- Truncar tablas antes de insertar `departamentos`
+-- Volcado de datos para la tabla `departamentos`
 --
 
-TRUNCATE TABLE `departamentos`;
+INSERT INTO `departamentos` (`id`, `nombre`, `id_facultad`) VALUES
+(1, 'Álgebra', 2),
+(2, 'ISIA', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -134,15 +125,40 @@ TRUNCATE TABLE `departamentos`;
 
 CREATE TABLE `facultades` (
   `id` int(2) NOT NULL,
-  `nombre` varchar(100) COLLATE latin1_spanish_ci NOT NULL,
-  `campus` varchar(100) COLLATE latin1_spanish_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+  `nombre` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `campus` varchar(100) COLLATE utf8_spanish2_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
--- Truncar tablas antes de insertar `facultades`
+-- Volcado de datos para la tabla `facultades`
 --
 
-TRUNCATE TABLE `facultades`;
+INSERT INTO `facultades` (`id`, `nombre`, `campus`) VALUES
+(1, '1', 'Moncola'),
+(2, '2', 'Somosaguas');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `itinerarios`
+--
+
+CREATE TABLE `itinerarios` (
+  `id` int(2) NOT NULL,
+  `id_carrera` int(2) NOT NULL,
+  `nombre` varchar(150) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `itinerarios`
+--
+
+INSERT INTO `itinerarios` (`id`, `id_carrera`, `nombre`) VALUES
+(1, 1, 'Computación'),
+(2, 1, 'Tecnología de la Información'),
+(3, 2, 'Itinerario Único'),
+(4, 3, 'Itinerario Único');
+
 --
 -- Índices para tablas volcadas
 --
@@ -185,26 +201,46 @@ ALTER TABLE `facultades`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `itinerarios`
+--
+ALTER TABLE `itinerarios`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id_carrera` (`id_carrera`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `carreras`
+--
+ALTER TABLE `carreras`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `clases`
 --
 ALTER TABLE `clases`
-  MODIFY `id` bigint(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `departamentos`
 --
 ALTER TABLE `departamentos`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `facultades`
 --
 ALTER TABLE `facultades`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `itinerarios`
+--
+ALTER TABLE `itinerarios`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
